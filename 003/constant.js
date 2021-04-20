6f5b83cb4448d86f090c8d3cc7d3684e65ffddfb
yuby <s30234d@naver.com>
2021-04-20 11:03:27 +0900

    feat: 상수관리

---
+++ 003/constant.js
@@ -0,0 +1,6 @@
+const ATTR_POSITION_NAME = 'a_position';
+const ATTR_POSITION_LOC = 0;
+const ATTR_NORMAL_NAME = 'a_normal';
+const ATTR_NORMAL_LOC = 1;
+const ATTR_UV_NAME = 'a_uv';
+const ATTR_UV_LOC = 2;
